24fcba6104a508fbe74209dde6f20bd6331fb990
Alexander <teichopsia@gmail.com>
2023-03-23 20:06:11 +0000

    add challenger class

---
+++ src/challenge.js
@@ -0,0 +1,37 @@
+/**
+ * gamify the challenge class, add logic for user votes on completed challenges. The creator of the recipe can either determine the winner, or leave it to other users to determine if the challenger won or did not win
+
+ */
+
+class Challenge {
+  constructor(createdBy, recipe, challenger) {
+    this.createdBy = createdBy
+    this.recipe = recipe
+    this.challenger = challenger
+    this.completed = false
+    this.winner = null
+    this.votes = []
+
+    this.challenger.addChallenge(this)
+    this.createdBy.addChallenge(this)
+  }
+
+  getChallenge() {
+    return {
+      createdBy: this.createdBy,
+      recipe: this.recipe,
+      challenger: this.challenger,
+      completed: this.completed,
+      winner: this.winner,
+      votes: this.votes,
+    }
+  }
+
+  addVote(vote) {
+    this.votes.push(vote)
+  }
+
+  getVotes() {
+    return this.votes
+  }
+}
