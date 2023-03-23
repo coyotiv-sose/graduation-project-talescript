7bc35bede30ab123ce808a6d0238e4014456fd1f
Alexander <teichopsia@gmail.com>
2023-03-23 13:36:54 +0000

    Add purpose of the app

---
+++ src/index.js
@@ -0,0 +1,37 @@
+/**
+ * Develop a web-base app that includes a sourdough calculator to
+ * eliminate the need for users to learn bakers math.
+ * The app will keep records of recipes and provide step-by-step
+ * instructions for each stage of the baking process.
+ * The app will allow users to add notes and keep track of
+ * temperatures and times.
+ * The app could further develop for professional bakers to keep
+ * track of recipes, quantities, ingredients and deliveries.
+ *
+ * Features:
+ * - Sourdough Calculator: bakers percentages, hydration, and quantities
+ * a recipe will yield.
+ * - Recipe Manager: keep track of recipes and ingredients, to make
+ * notes about each recipe. Invetory manager, recipe costing, and delivery tracking.
+ * - Temperature and Time Tracker: keep track of temperatures and times of baking process.
+ * - Step-by-Step Instructions: provide step-by-step instructions for each stage of the baking process.
+ *
+ * Gamification:
+ * - Achievements and badges: earn badges for completing tasks. Such as mastering a particular bread recipe
+ * or baking consistently for a certain amount of time.
+ * - Leaderboards: compete with other bakers to see who can bake the best bread. Encourage friendly competition
+ * and sharing of recipes. Earn points for completing recipes, sharing their experiences, and participating
+ * in challenges.
+ * - Challenge friends or other users to bake a particular recipe. Then vote on the best result.
+ *
+ * Sharing: Users can share their recipes and experiences with other users, tips and tricks and photos.
+ *
+ * Interactivity ideas:
+ * - Discussion forums
+ * - Live chat
+ * - User-generated content
+ * - Challenges and competitions
+ * - Virtual events
+ */
+
+/**  */
