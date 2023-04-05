84ec0c79727d02b9cea043a302755562604257ac
talescript <hello.talescript@gmail.com>
2023-04-05 20:51:46 +0200

    no idea. Just pushing cause everything is working

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,14 +4,14 @@ var User = require('../models/user')
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.send([{ name: 'john' }]) // change url to /users
+  res.send([{ name: 'john' }, {name: 'jane'}]) // change url to /users
 })
 
-router.post('/', function (req, res, next) {
+/* router.post('/', function (req, res, next) {
   const newUser = User.create(req.body.name, req.body.email);
   res.send(newUser)
 })
-
+ */
 
 
 module.exports = router
