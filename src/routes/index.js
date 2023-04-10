ed9142b28c153a02bac3274ad30ee5aca0753469
talescript <hello.talescript@gmail.com>
2023-04-10 22:28:54 +0200

    add a comment to understand how render works

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // rendering the index view
   res.render('index', { title: 'SourdoughAPP' });
 });
 
