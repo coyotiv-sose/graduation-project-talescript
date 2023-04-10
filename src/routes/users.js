f3b3988a526a4ce309b005007486e5db81a47ee9
talescript <hello.talescript@gmail.com>
2023-04-10 22:28:21 +0200

    Add routes for users

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,7 +4,29 @@ var User = require('../models/user')
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.send([{ name: 'john' }, {name: 'jane'}]) // change url to /users
+
+  res.send(User.list) // change url to /users
+  return
+
+  res.render('users', {
+    user: {
+      name: 'Hello dude',
+    },
+    users: [
+      {name: 'Justin'},
+      {name: 'Sarah'},
+      {name: 'Maria'},
+      {name: 'dumbo'},
+    ]
+  })
+
+})
+
+router.post('/', (req, res, next) => {
+  //const user = new User(req.body.name) // gets it from axios post
+  // new method created in user.js static
+  const user = User.create({name: req.body.name, email: req.body.email})
+  res.send({user})
 })
 
 /* router.post('/', function (req, res, next) {
@@ -12,6 +34,5 @@ router.get('/', function (req, res, next) {
   res.send(newUser)
 })
  */
-
 
 module.exports = router
