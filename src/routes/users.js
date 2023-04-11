941e4a38ee8809f16acecfc8a3f2c4b9d607453b
Alexander <teichopsia@gmail.com>
2023-04-11 19:53:26 +0000

    minor formatting

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,7 +4,6 @@ var User = require('../models/user')
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-
   res.send(User.list) // change url to /users
   return
 
@@ -12,21 +11,15 @@ router.get('/', function (req, res, next) {
     user: {
       name: 'Hello dude',
     },
-    users: [
-      {name: 'Justin'},
-      {name: 'Sarah'},
-      {name: 'Maria'},
-      {name: 'dumbo'},
-    ]
+    users: [{ name: 'Justin' }, { name: 'Sarah' }, { name: 'Maria' }, { name: 'dumbo' }],
   })
-
 })
 
 router.post('/', (req, res, next) => {
   //const user = new User(req.body.name) // gets it from axios post
   // new method created in user.js static
-  const user = User.create({name: req.body.name, email: req.body.email})
-  res.send({user})
+  const user = User.create({ name: req.body.name, email: req.body.email })
+  res.send({ user })
 })
 
 /* router.post('/', function (req, res, next) {
@@ -40,10 +33,10 @@ router.post('/', (req, res, next) => {
 //     const user = User.list.find(user => user.name === req.params.userId)
 
 //     const recipe = user.createRecipe({
-//       title: req.body.title, 
-//       date: req.body.date, 
+//       title: req.body.title,
+//       date: req.body.date,
 //       ingredients: req.body.ingredients})
-//     // because of a circular error, send directly instead of the recipe 
+//     // because of a circular error, send directly instead of the recipe
 //     res.send({title: recipe.name, date: recipe.date, ingredients: recipe.ingredients})
 //   } catch (err) {
 //     res.status(400).send(err.message)
