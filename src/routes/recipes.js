a2e097ad54e90533110ec6209cbab6beb3f435a6
Alexander <teichopsia@gmail.com>
2023-04-12 16:14:55 +0000

    add post route for creating a recipe

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const Recipe = require('../models/recipe')
+const User = require('../models/user')
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
+  res.send(Recipe.list)
+})
 
-    res.send(Recipe.list) 
-  
-  })
+router.post('/', async (req, res, next) => {
+  const user = await User.findById(req.body.user)
+  const newRecipe = await user.createRecipe(req.body.title, req.body.ingredients)
+  res.send(newRecipe)
+})
 
-  
-  module.exports = router
+module.exports = router
