6106c3dd8ad936b3b0066e40f000e80ee12e6ca3
Alexander <teichopsia@gmail.com>
2023-04-12 16:14:09 +0000

    add mongoose schema

--- src/models/recipe.js
+++ src/models/recipe.js
@@ -1,36 +1,40 @@
+const mongoose = require('mongoose')
+
+const recipeSchema = new mongoose.Schema(
+  {
+    title: String,
+    ingredients: Array,
+    notes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Note' }],
+  },
+  { timestamps: true }
+)
 class Recipe {
-    notes = []
-  
-    //date = new Date
-    constructor(title, date, ...ingredients) {
-      this.title = title
-      this.date = date
-      this.ingredients = ingredients
-    }
-  
-    // demo of how to use getter in alexander.recipes[0].recipeDetails
-    get recipeDetails() {
-      return `
+  // dont need this since we are using mongoose
+  // notes = []
+
+  // //date = new Date
+  // constructor(title, date, ...ingredients) {
+  //   this.title = title
+  //   this.date = date
+  //   this.ingredients = ingredients
+  // }
+
+  // demo of how to use getter in alexander.recipes[0].recipeDetails
+  get recipeDetails() {
+    return `
       recipe title: ${this.title}
       recipe date: ${this.date}
       recipe ingredients: ${this.ingredients.join(' ')}`
-  
-      // const map1 = array1.map(x => x * 2);
-    }
-  
-    // edit recipe
-  
-    // delete recipe
-  
-    // share recipe`
 
-    static create(title, date, ...ingredients) {
-      const newRecipe = new Recipe(title, date, ...ingredients)
-      Recipe.list.push(newRecipe)
-      return newRecipe
-    }
-
-    static list = []
+    // const map1 = array1.map(x => x * 2);
   }
-  
-  module.exports = Recipe;
+
+  // edit recipe
+
+  // delete recipe
+
+  // share recipe`
+}
+
+recipeSchema.loadClass(Recipe)
+module.exports = mongoose.model('Recipe', recipeSchema)
