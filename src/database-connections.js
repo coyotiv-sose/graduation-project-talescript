daca2e2a301a694a18d4a4d72d703ed906edbba8
Alexander <teichopsia@gmail.com>
2023-04-12 16:13:15 +0000

    change file name

--- src/database-connections.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING)
-
-const Cat = mongoose.model('Cat', { name: String })
-
-const kitty = new Cat({ name: 'Zildjian' })
-kitty.save().then(() => console.log('meow'))
-
-Cat.create({name: 'asd'}).then(() => console.log('meow'))
