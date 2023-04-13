c1cd5725723f2cc50040faa3a3276f66e24a4650
Alexander <teichopsia@gmail.com>
2023-04-13 18:55:05 +0000

    formatting maybe.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   // rendering the index view
-  res.render('index', { title: 'SourdoughAPP' });
-});
+  res.render('index', { title: 'SourdoughAPP' })
+})
 
-module.exports = router;
-
-
+module.exports = router
