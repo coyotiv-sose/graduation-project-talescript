0308f95d86d822c65051c77c7196864b5033cd81
Alexander <teichopsia@gmail.com>
2023-04-17 20:12:31 +0000

    create a course

---
+++ backend/src/models/course.js
@@ -0,0 +1,11 @@
+class Course {
+  courses = []
+
+  constructor(title, description, price, startDate, endDate) {
+    this.title = title
+    this.description = description
+    this.price = price
+    this.startDate = startDate
+    this.endDate = endDate
+  }
+}
