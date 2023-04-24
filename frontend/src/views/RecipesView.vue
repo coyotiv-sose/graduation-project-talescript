db47359512f1fc19583d75f4b9f3f8bb3aa94dfe
Alexander <teichopsia@gmail.com>
2023-04-24 21:55:42 +0000

    add a recipe view / route

---
+++ frontend/src/views/RecipesView.vue
@@ -0,0 +1,31 @@
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'RecipesView',
+  data() {
+    return {
+      recipes: [],
+      name: 'alexander'
+    }
+  },
+  async created() {
+    const response = await axios.get('http://localhost:3000/recipes')
+    this.recipes = response.data
+  },
+  methods: {
+    async changeName(name) {
+      this.name = 'alex'
+    }
+  }
+}
+</script>
+
+<template lang="pug">
+main
+  h1 Recipes
+  p {{ name }}
+  button(@onClick="changeName") Change name
+  ul
+    li(v-for="recipe in recipes") {{ recipe.title }}
+</template>
