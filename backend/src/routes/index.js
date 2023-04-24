1dfa576e2986812fc88d588d9df2dbbe0d922b6a
Alexander <teichopsia@gmail.com>
2023-04-24 07:40:47 +0000

    create a 404 for non existent urls

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -7,4 +7,18 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'SourdoughAPP' })
 })
 
+// return 404 if no route is found
+router.use(function (req, res, next) {
+  res.status(404).send('Sorry cant find that!')
+})
+
+// login
+router.post('/login', function (req, res, next) {
+  // check if user exists
+  // if user exists, check if password is correct
+  // if password is correct, send back user
+  // if password is incorrect, send back error
+  // if user does not exist, send back error
+})
+
 module.exports = router
