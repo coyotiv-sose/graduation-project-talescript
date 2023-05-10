70edb7593d04ed91fcdaad08cef39cba4acb5b0e
Alexander <teichopsia@gmail.com>
2023-05-10 20:56:37 +0000

    add account route

--- backend/src/routes/accounts.js
+++ backend/src/routes/accounts.js
@@ -3,4 +3,19 @@ const router = express.Router()
 const passport = require('passport')
 const User = require('../models/user')
 
-// create account
+router.get('/session', async function (req, res, next) {
+  console.log('the current user is', req.user)
+  res.send(req.session)
+})
+
+router.post('/session', passport.authenticate('local', { failWithError: true }), function (req, res, next) {
+  res.send(req.user)
+})
+
+router.delete('/session', async function (req, res) {
+  req.logout(() => {
+    res.sendStatus(200)
+  })
+})
+
+module.exports = router
