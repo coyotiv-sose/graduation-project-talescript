d81bb88631246c0382ae7ea14e59b37bc82e47bb
Alexander <teichopsia@gmail.com>
2023-05-13 23:31:46 +0000

    code cleanup to make it easier to read

--- backend/src/database-connection.js
+++ backend/src/database-connection.js
@@ -1,2 +1,4 @@
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING).then(() => console.log('Connected to MongoDB'))
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING)
+  .then(() => console.log('Connected to MongoDB', process.env.MONGODB_CONNECTION_STRING))
