cf7c223103fcdf23d83736ab5991e73c681ec917
Alexander <teichopsia@gmail.com>
2023-05-21 13:27:40 +0000

    add calculator page to the home page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,8 @@
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 import { useAccountStore } from './stores/account'
-import { useSocketStore } from './stores/socket'
+import CreatePage from './components/CalculatorPage.vue'
+// import { useSocketStore } from './stores/socket'
 
 import { mapActions, mapState } from 'pinia'
 
@@ -15,15 +16,32 @@ export default {
   },
   async mounted() {
     await this.fetchUser()
-    this.init()
+    // this.init()
   },
   methods: {
     ...mapActions(useAccountStore, ['fetchUser', 'logout']),
-    ...mapActions(useSocketStore, ['init'])
+    // ...mapActions(useSocketStore, ['init'])
+    async createRecipe() {
+      let res = await fetch('pages.json') // fetch('http://localhost:3000/recipes') isntead of json
+      // let res = await fetch('http://localhost:3000/recipes', {
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type': 'application/json'
+      //   },
+      //   body: JSON.stringify({
+      //     title: 'Sourdough',
+      //     description: 'Sourdough bread',
+      //     ingredients: ['flour', 'water', 'salt'],
+      //     instructions: 'Mix ingredients, let sit, bake'
+      //   })
+      // })
+      let data = await res.json()
+      this.pageCreated = data
+    }
   },
   computed: {
-    ...mapState(useAccountStore, ['user']),
-    ...mapState(useSocketStore, ['connected'])
+    ...mapState(useAccountStore, ['user'])
+    // ...mapState(useSocketStore, ['connected'])
   }
 }
 </script>
@@ -34,25 +52,30 @@ export default {
       <HelloWorld msg="Sourdough" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/recipes">Recipes</RouterLink>
-        <RouterLink to="/events">Events</RouterLink>
-        <RouterLink v-if="!user" to="/login">Log in</RouterLink>
-        <a href="#" rel="noopener noreferrer" v-if="user" @click="logout">Logout</a>
+        <ul>
+          <li><strong>brand</strong></li>
+        </ul>
+        <ul>
+          <li><RouterLink to="/">Home</RouterLink></li>
+          <li><RouterLink to="/about">About</RouterLink></li>
+          <li><RouterLink to="/recipes">Recipes</RouterLink></li>
+          <li><RouterLink to="/events">Events</RouterLink></li>
+          <li><RouterLink v-if="!user" to="/login">Log in</RouterLink></li>
+          <li><a href="#" rel="noopener noreferrer" v-if="user" @click="logout">Logout</a></li>
 
-        <RouterLink v-if="!user" to="/signup">Sign up</RouterLink>
+          <li><RouterLink v-if="!user" to="/signup">Sign up</RouterLink></li>
+        </ul>
       </nav>
     </div>
   </header>
-  <h1>sourdough {{ user?.name }} socket? {{ connected ? 'yes' : 'no' }}</h1>
+  <!-- <h1>sourdough {{ user?.name }} socket? {{ connected ? 'yes' : 'no' }}</h1> -->
 
   <Suspense>
     <RouterView />
   </Suspense>
 </template>
 
-<style scoped>
+<!-- <style scoped>
 header {
   line-height: 1.5;
   max-height: 100vh;
@@ -114,4 +137,4 @@ nav a:first-of-type {
     margin-top: 1rem;
   }
 }
-</style>
+</style> -->
