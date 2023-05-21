9dacaf7471e4444c4c4c0cc1ae46d412aee6a5f0
Alexander <teichopsia@gmail.com>
2023-05-21 13:23:35 +0000

    create a calculator component

---
+++ frontend/src/components/CalculatorPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <form action="" method="get">
+      <div>
+        <label for=""> Page title </label>
+        <input type="text" name="" v-model="PageTitle" />
+      </div>
+      <div>
+        <label for=""> Content </label>
+        <textarea name="" id="" rows="5"></textarea>
+      </div>
+      <div>
+        <button @click.prevent="pageCreated({ PageTitle })">Create recipe</button>
+      </div>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    pageCreated: {
+      type: Object,
+      default(rawProps) {
+        return {
+          PageTitle: rawProps.PageTitle || '',
+          content: rawProps.content || ''
+        }
+      }
+    }
+  },
+  data() {
+    return {
+      PageTitle: ''
+    }
+  }
+}
+</script>
