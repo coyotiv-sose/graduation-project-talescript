a32457e22c77da4b59a953ab085b021b4b00d711
Alexander <teichopsia@gmail.com>
2023-05-22 19:42:41 +0000

    add bakers calculator

--- frontend/src/components/CalculatorPage.vue
+++ frontend/src/components/CalculatorPage.vue
@@ -1,57 +1,89 @@
 <script>
-// const recipeCreator = require('bakers-math')
+import axios from 'axios'
+import { bakersMath } from 'bakers-math'
 
 export default {
   data() {
     return {
-      product: '',
+      product: 'sdsds',
       individualWeight: 0,
       quantity: 0,
       totalPercentage: 0,
       totalWeight: 0,
       flourWeight: 0,
-      ingredients: [
-        { name: 'Water', weight: 0 },
-        { name: 'Yeast', weight: 0 },
-        { name: 'Salt', weight: 0 },
-        { name: 'Sugar', weight: 0 },
-        { name: 'Sourdough', weight: 0 }
-      ]
+      ingredient: {
+        name: '',
+        weight: 0
+      },
+      ingredients: []
+    }
+  },
+  methods: {
+    async submitRecipe() {
+      // change to axiosURL
+      const response = await axios.post('http://localhost:3000/recipes', {
+        name: this.product,
+        // individualWeight: this.individualWeight,
+        // quantity: this.quantity,
+        // totalPercentage: this.totalPercentage,
+        // totalWeight: this.totalWeight,
+        // flourWeight: this.flourWeight,
+        ingredients: this.ingredients
+      })
+      this.$router.push(`/recipes/${response.data._id}`)
+    },
+    addIngredient(e) {
+      this.ingredients.push({ name: this.ingredient.name, weight: this.ingredient.weight })
+      this.ingredient.name = ''
+      this.ingredient.weight = 0
+      e.preventDefault()
     }
   }
 }
-//   {
-//   product: 'buns',
-//   individualWeight: 200,
-//   quantity: 4,
-//   totalPercentage: 176.4,
-//   totalWeight: 800,
-//   flourWeight: 454,
-//   ingredients: [
-//     { name: 'Water', weight: 544 },
-//     { name: 'Yeast', weight: 13 },
-//     { name: 'Salt', weight: 18 },
-//     { name: 'Sugar', weight: 45 },
-//     { name: 'Sourdough', weight: 73 }
-//   ]
-// }
-// }
 </script>
 
 <template>
   <div>
-    <form action="" method="post">
-      <div>
-        <label for=""> {{ product }} </label>
-        <input type="text" name="" v-model="product" />
+    <form @submit.prevent="submitRecipe">
+      <div class="grid">
+        <div>
+          <label for="product"> Name </label
+          ><!--req.body.product backend-->
+          <input type="text" name="product" id="product" v-model="product" />
+        </div>
+        <div>
+          <label for="quantity">Quantity</label>
+          <input type="number" name="quantity" id="quantity" v-model="quantity" />
+        </div>
       </div>
+
       <div>
-        <label for=""> Content </label>
-        <textarea name="" id="" rows="5"></textarea>
+        <label for="ingredient"> Add ingredient </label>
+        <input type="text" name="ingredient" id="ingredient" v-model="ingredient.name" />
+        <input type="number" v-model="ingredient.weight" />
       </div>
+      <button @click="addIngredient">Add Ingredient</button>
       <div>
-        <button @click.prevent="pageCreated({})">Create recipe</button>
+        <button type="submit">Create recipe</button>
       </div>
     </form>
+
+    <section>
+      <h2>Recipe</h2>
+      <div>
+        <p>Product: {{ product }}</p>
+        <p>Quantity: {{ quantity }}</p>
+        <p>Individual weight: {{ individualWeight }}</p>
+        <p>Total percentage: {{ totalPercentage }}</p>
+        <p>Total weight: {{ totalWeight }}</p>
+        <p>Flour weight: {{ flourWeight }}</p>
+        <p>Ingredients:</p>
+        <ul>
+          <li v-for="ingredient in ingredients" :key="ingredient._id">
+            {{ ingredient.name }}: {{ ingredient.weight }}
+          </li>
+        </ul>
+      </div>
+    </section>
   </div>
 </template>
