f31e822e1ff6c4d6a525a4d574d12169e83c60c1
Alexander <teichopsia@gmail.com>
2023-05-22 15:48:59 +0000

    remove boilerplate code

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -22,7 +22,7 @@ export default {
     ...mapActions(useAccountStore, ['fetchUser', 'logout']),
     // ...mapActions(useSocketStore, ['init'])
     async createRecipe() {
-      let res = await fetch('pages.json') // fetch('http://localhost:3000/recipes') isntead of json
+      // fetch('http://localhost:3000/recipes') isntead of json
       // let res = await fetch('http://localhost:3000/recipes', {
       //   method: 'POST',
       //   headers: {
@@ -35,8 +35,6 @@ export default {
       //     instructions: 'Mix ingredients, let sit, bake'
       //   })
       // })
-      let data = await res.json()
-      this.pageCreated = data
     }
   },
   computed: {
@@ -49,8 +47,6 @@ export default {
 <template>
   <header>
     <div class="wrapper">
-      <HelloWorld msg="Sourdough" />
-
       <nav>
         <ul>
           <li><strong>brand</strong></li>
@@ -69,72 +65,8 @@ export default {
     </div>
   </header>
   <!-- <h1>sourdough {{ user?.name }} socket? {{ connected ? 'yes' : 'no' }}</h1> -->
-
+  <HelloWorld msg="Sourdough" />
   <Suspense>
     <RouterView />
   </Suspense>
 </template>
-
-<!-- <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style> -->
--- frontend/src/components/CalculatorPage.vue
+++ frontend/src/components/CalculatorPage.vue
@@ -1,37 +1,57 @@
+<script>
+// const recipeCreator = require('bakers-math')
+
+export default {
+  data() {
+    return {
+      product: '',
+      individualWeight: 0,
+      quantity: 0,
+      totalPercentage: 0,
+      totalWeight: 0,
+      flourWeight: 0,
+      ingredients: [
+        { name: 'Water', weight: 0 },
+        { name: 'Yeast', weight: 0 },
+        { name: 'Salt', weight: 0 },
+        { name: 'Sugar', weight: 0 },
+        { name: 'Sourdough', weight: 0 }
+      ]
+    }
+  }
+}
+//   {
+//   product: 'buns',
+//   individualWeight: 200,
+//   quantity: 4,
+//   totalPercentage: 176.4,
+//   totalWeight: 800,
+//   flourWeight: 454,
+//   ingredients: [
+//     { name: 'Water', weight: 544 },
+//     { name: 'Yeast', weight: 13 },
+//     { name: 'Salt', weight: 18 },
+//     { name: 'Sugar', weight: 45 },
+//     { name: 'Sourdough', weight: 73 }
+//   ]
+// }
+// }
+</script>
+
 <template>
   <div>
-    <form action="" method="get">
+    <form action="" method="post">
       <div>
-        <label for=""> Page title </label>
-        <input type="text" name="" v-model="PageTitle" />
+        <label for=""> {{ product }} </label>
+        <input type="text" name="" v-model="product" />
       </div>
       <div>
         <label for=""> Content </label>
         <textarea name="" id="" rows="5"></textarea>
       </div>
       <div>
-        <button @click.prevent="pageCreated({ PageTitle })">Create recipe</button>
+        <button @click.prevent="pageCreated({})">Create recipe</button>
       </div>
     </form>
   </div>
 </template>
-<script>
-export default {
-  props: {
-    pageCreated: {
-      type: Object,
-      default(rawProps) {
-        return {
-          PageTitle: rawProps.PageTitle || '',
-          content: rawProps.content || ''
-        }
-      }
-    }
-  },
-  data() {
-    return {
-      PageTitle: ''
-    }
-  }
-}
-</script>
