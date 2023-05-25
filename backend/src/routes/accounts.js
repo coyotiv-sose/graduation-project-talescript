b1948ba9a62d9cbc03669909c9d50ff63de6d325
Alexander <teichopsia@gmail.com>
2023-05-25 08:16:44 +0000

    add a registration route to accounts

--- backend/src/routes/accounts.js
+++ backend/src/routes/accounts.js
@@ -8,6 +8,17 @@ router.get('/session', async function (req, res, next) {
   res.send(req.user)
 })
 
+router.post('/register', async function (req, res, next) {
+  const { name, email, password } = req.body
+  try {
+    const user = await User.register({ name, email }, password)
+    res.send(user)
+  } catch (error) {
+    // Handle registration error
+    res.status(400).send({ error: 'Registration failed' })
+  }
+})
+
 // router.post('/', async function (req, res, next) {
 //   const { name, email, password } = req.body
 //   const user = await User.register({ name, email }, password)
